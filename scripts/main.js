19ae010bda4802669ef17d737bf7367266b46a60
Chad Fawcett <me@chadf.ca>
2015-12-24 15:45:53 -0800

    End of video 11

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,11 @@ var App = React.createClass({
     this.state.fishes['fish-' + timestamp] = fish;
     this.setState({ fishes: this.state.fishes });
   },
+  loadSamples: function () {
+    this.setState({
+      fishes: require('./sample-fishes')
+    });
+  },
   render: function () {
     return (
       <div className="catch-of-the-day">
@@ -33,7 +38,7 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
@@ -114,6 +119,7 @@ var Inventory = React.createClass({
       <div>
         <h2>Inventory</h2>
         <AddFishForm {...this.props} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
   }
